301fca8cb58d355e0e163485c8171ef7606a24d9
SamyajitD <122200049+SamyajitD@users.noreply.github.com>
2023-07-04 04:50:17 +0530

    swing version using JOptionPane

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,11 +1,19 @@
 //Task 2-Number Guessing Game
 //package NumberGuesingGame
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class NumberGuessingGame {
 
     static Scanner sc = new Scanner(System.in);
+    static JFrame frame = new JFrame();
     int randomNumber;
     int scoreAllotted;
     int round;
@@ -19,36 +27,35 @@ public class NumberGuessingGame {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hey There!!\nWanna Play a Game with Me!!\n");
+        NumberGuessingGame guessGame = new NumberGuessingGame();
         while (true) {
             try {
                 // For First Time Play
-                System.out.println("Press\n\n1 . Sure Lets Plays!\t2 . Not Right Now\n");
-                NumberGuessingGame guessGame = new NumberGuessingGame();
-                int play = sc.nextInt();
+                String input = JOptionPane.showInputDialog(frame,
+                        "Hey There!!\nWanna Play A Game With Me!!\nPress\n1 . Sure Lets Plays!\n2 . Not Right Now");
+                if (input == null) {
+                    guessGame.exit();
+                }
+                int play = Integer.parseInt(input);
+
                 switch (play) {
                     case 1:
-                        System.out.println("Great\n");
                         // Explanation of the Game
-                        System.out.println("____________________________________________________");
-                        System.out.println(
-                                "I Will Choose A Number You Will Have To Guess It!!\n");
-                        System.out.println(
-                                "You will have a total of & attempts.\n\nDon't Worry I will help you along the way haha.\n");
-                        System.out.println("____________________________________________________\n");
-                        System.out.println("Now lets Begin.\n");
+                        JOptionPane.showMessageDialog(frame,
+                                "Great\nI Will Choose A Number You Will Have To Guess It!!\nYou will have a total of & attempts.\nDon't Worry I will help you along the way haha.\nNow lets Begin.");
+
                         guessGame.getRandomNumber();
                         guessGame.game();
                         break;
                     case 2:
-                        System.out.println("Oh Thats Too Bad. See You Later Then !!\n");
+                        JOptionPane.showMessageDialog(frame, "Oh Thats Too Bad. See You Later Then !!\n");
                         guessGame.exit();
                         break;
                     default:
-                        throw new InputMismatchException("Invalid Choice!! Please Try Again");
+                        throw new InputMismatchException("Invalid Choice!! Please Try Again!");
                 }
             } catch (InputMismatchException e) {
-                System.err.println("\n" + e.getMessage() + "\n\n");
+                JOptionPane.showMessageDialog(frame, e.getMessage());
 
             }
         }
@@ -59,23 +66,26 @@ public class NumberGuessingGame {
     public void game() {
         scoreAllotted = 10;
         while (scoreAllotted > 3) {
-            System.out.println("Enter The Number You Have Guessed");
-            int num = sc.nextInt();
+            String input = JOptionPane.showInputDialog(frame, "Enter your guess");
+            if (input == null) {
+                exit();
+            }
+            int num = Integer.parseInt(input);
             if (num == randomNumber) {
-                System.out.println("CONGRATS!! You Guessed Right");
+                JOptionPane.showMessageDialog(frame, "CONGRATS!! You Guessed Right");
                 scoreBoard.put(round, scoreAllotted);
                 break;
             }
             if (num > randomNumber) {
-                System.out.println("Guess Lower");
+                JOptionPane.showMessageDialog(frame, "Guess Lower");
             } else {
-                System.out.println("Guess Higher");
+                JOptionPane.showMessageDialog(frame, "Guess Higher");
             }
 
             scoreAllotted--; // score is deduced for every wrong attempt
         }
         if (scoreAllotted == 3) {
-            System.out.println("\nSeems Like I Win This Roung Haha.\n");
+            JOptionPane.showMessageDialog(frame, "\nSeems Like I Win This Roung Haha.\n");
             scoreBoard.put(round, 0);
         }
         round++;
@@ -86,11 +96,10 @@ public class NumberGuessingGame {
     public void menu() {
 
         try {
-            System.out.println("What Would you like to do!!");
-            System.out.println("1 . Play Again!");
-            System.out.println("2 . View ScoreBoard");
-            System.out.println("3 . Exit");
-            int option = sc.nextInt();
+
+            String input = JOptionPane.showInputDialog(frame,
+                    "What Would you like to do!!\n1 . Play Again!\n2 . View ScoreBoard\n3 . Exit");
+            int option = Integer.parseInt(input);
             switch (option) {
                 case 1:
                     getRandomNumber();
@@ -101,13 +110,13 @@ public class NumberGuessingGame {
                     menu();
                     break;
                 case 3:
-                    System.out.println("Thank You For Playing");
+                    JOptionPane.showMessageDialog(frame, "Thank You For Playing");
                     exit();
                 default:
                     throw new InputMismatchException("Invalid Choice!\n");
             }
         } catch (InputMismatchException e) {
-            System.err.println("\n" + e.getMessage() + "\n");
+            JOptionPane.showMessageDialog(frame, e.getMessage());
             menu();
         }
 
@@ -115,18 +124,22 @@ public class NumberGuessingGame {
 
     // to generate the random number in user input bounds
     public void getRandomNumber() {
-        System.out.println("Enter The Upper Limit For The Mysterious Number (i.e. form 1-X )");
-        int range = sc.nextInt();
+        String input = JOptionPane.showInputDialog(frame,
+                "Enter The Upper Limit For The Mysterious Number (i.e. form 1-X )");
+        if (input == null) {
+            exit();
+        }
+        int range = Integer.parseInt(input);
         Random generator = new Random();
         randomNumber = generator.nextInt(range) + 1;
     }
 
     // to view ScoreBoard
     public void getScoreBoard() {
-        System.out.println("\tScoreBoard!!\n");
-        System.out.println("\tRound\tScore");
-        scoreBoard.forEach((k, v) -> System.out.println("\t  " + k + "\t  " + v));
-        System.out.println();
+        StringBuilder scores = new StringBuilder();
+        scoreBoard.forEach((k, v) -> scores.append("\t     " + k + "               " + v + "\n"));
+        JOptionPane.showMessageDialog(frame, "\t  ScoreBoard!!\n\tRound      Score\n" + scores.toString());
+
     }
 
     // exits the program
